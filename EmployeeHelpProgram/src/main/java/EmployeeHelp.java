667b15e15cc9c5a3c993f881b8d8ee9ec1dbaa40
ysuranouswiir@yahoo.com <ysuranouswiir@yahoo.com>
2021-06-30 20:52:30 -0500

    Maven Env Setup & Basic demo screen with button

--- EmployeeHelpProgram/src/main/java/EmployeeHelp.java
+++ EmployeeHelpProgram/src/main/java/EmployeeHelp.java
@@ -1,9 +1,33 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class EmployeeHelp {
-
+public class EmployeeHelp extends Application{
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) throws Exception {
+		
+		Button button;
+		
+		primaryStage.setTitle("Employee Help Program");
+		
+		button = new Button();
+		button.setText("Click me");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 
 }
